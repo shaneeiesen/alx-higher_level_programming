a19c16f31415b0f9ead99efdd8a06445a57d6f14
shaneeisen <shanetech.it@gmail.com>
2024-03-20 23:18:12 +0000

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+dd(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
